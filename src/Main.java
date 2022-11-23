08fe88289d7043fb2a0a9dd01b180087df51b965
Alexey <ReetzCraft@gmail.com>
2022-11-23 13:58:25 +0300

    third part of homework

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         //task 1
-        int total = 15_000;
-        int monthNumber = 0;
-        while (total < 12_000_000) {
-            total = total + total * 7 / 100;
-            monthNumber++;
-            System.out.println("Месяц " + monthNumber + ", сумма накоплений равна " + total);
+        int currentYear = 2022;
+        int bottomLine = currentYear - 200;
+        int topLine = currentYear + 100;
+        for (int i = bottomLine; i <= topLine; i++) {
+            if (i % 79 == 0) {
+                System.out.println(i);
+            }
         }
-        System.out.println("Всего месяцев потребовалось " + monthNumber);
 
         //task 2
-        total = 15_000;
-        monthNumber = 0;
-        while (total < 12_000_000) {
-            total = total + total * 7 / 100;
-            monthNumber++;
-            if (monthNumber % 6 == 0) {
-                System.out.println("Месяц " + monthNumber + ", сумма накоплений равна " + total);
-            }
-        }
-        System.out.println("Всего месяцев потребовалось " + monthNumber);
-
-        //task 3
-        total = 15_000;
-        int years = 9;
-        int monthsTotal = years * 12;
-        monthNumber = 0;
-        while (monthNumber < monthsTotal) {
-            total = total + total * 7 / 100;
-            monthNumber++;
-            if (monthNumber % 6 == 0) {
-                System.out.println("Полугодие " + monthNumber / 6 + " сумма " + total);
-            }
-        }
-
-        //task 4
-        int firstFridayOfMonth = 4;
-        while (firstFridayOfMonth < 31) {
-            System.out.println("Сегодня пятница, " + firstFridayOfMonth + "-е число. Необходимо подготовить отчет.");
-            firstFridayOfMonth += 7;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2*" + i + "=" + (2 * i));
         }
     }
 }
